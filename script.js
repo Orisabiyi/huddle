8edbe1d41b057ab05bc3292ce730f5d9cb8b65a4
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-03 12:59:43 +0100

    restructured the footer html structure

--- script.js
+++
@@ -1,9 +0,0 @@
-// Wait for the SVG to be loaded before styling
-document.querySelector('.footer__logo').addEventListener('load', function () {
-  const svgEl = this.contentDocument;
-
-  const pathElements = svgEl.getElementsByTagName('path');
-  for (let i = 0; i < pathElements.length; i++) {
-    pathElements[i].style.fill = '#fff';
-  }
-});
